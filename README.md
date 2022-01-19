ff4bcb77562b570cac563a369e33a8dd54d37b75
phooky <phooky@gmail.com>
2022-01-18 22:15:21 -0500

    expanding readme a bit, and whatever i happened to be doing to mkfont last time I poked my head in here

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Tools and information concerning the reverse engineering of a control panel from an Epson printer we found on the street
+EpsonCP
+=======
+
+Tools and information concerning the reverse engineering of a control panel from an Epson printer we found on the street.
+
+The target controller is a Raspberry Pi Pico. You can find the pin mapping from the Pico to the control panel's flat flexible cable (FFC) in the `pins.h` file. You can run the panel off the Pico's 3.3V regulated output, but you'll need to find a way to generate 1.8V for pin 2 on the FFC if you're using the LCD panel.
+
+The file `LCD-reversing.md` contains everything I was able to figure out about driving the LCD panel.
 
--- mkfont.py
+++ mkfont.py
@@ -15,18 +15,67 @@ if lookup_result.returncode != 0:
     print("Couldn't find font, exiting")
     sys.exit(1)
 
+class Glyph:
+    def __init__(self,w,h,left,top,bitmap):
+        self.w = w
+        self.h = h
+        self.left = left
+        self.top = top
+        self.bitmap = bitmap
+        self.aspect = 1
+
+    def even_h(self):
+        if (self.h%2) == 1:
+            self.h = self.h + 1
+            self.top = self.top + 1
+            self.bitmap = ([0] * self.w) + self.bitmap
+
+    def squash(self):
+        self.h = int(self.h/2)
+        bm = [0] * int(self.w * self.h)
+        for y in range(self.h):
+            for x in range(self.w):
+                bm[x+y*self.w] = 0
+                bm[x+y*self.w] = int( (self.bitmap[x+(y*2*self.w)] +
+                                       self.bitmap[x+((y*2+1)*self.w)])/2)
+        self.aspect = 2
+        self.bitmap = bm
+
+    def reduce_colors(self):
+        self.bitmap = [ (x + 0x1f) >> 6 for x in self.bitmap ]
+    
+    def print(self):
+        print("len {} x {} y {} left {} top {}".format(len(self.bitmap),self.w,self.h,
+                                                       self.left, self.top))
+        for y in range(self.h):
+            for x in range(self.w):
+                print("{:4}".format(self.bitmap[x + self.w*y]),end='')
+            if y == face.glyph.bitmap_top - 1:
+                print(" -- baseline")
+            else:
+                print()
+
+    def print_c(self):
+        pass
+        
+
+
+
 font_path = lookup_result.stdout;
 face = freetype.Face(font_path)
-face.set_char_size( 16*64 )
-for c in 'Sg':
+face.set_char_size( 18*64 )
+for c in 'S':
     face.load_char(c)
     bitmap = face.glyph.bitmap
-    print("len {} x {} y {} left {} top {}".format(len(bitmap.buffer),bitmap.width,bitmap.rows,
-                                                   face.glyph.bitmap_left,face.glyph.bitmap_top))
-    for y in range(bitmap.rows):
-        for x in range(bitmap.width):
-            print("{:4}".format(bitmap.buffer[x + bitmap.width*y]),end='')
-        if y == face.glyph.bitmap_top - 1:
-            print(" -- baseline")
-        else:
-            print()
+    g = Glyph(bitmap.width,bitmap.rows,
+              face.glyph.bitmap_left,
+              face.glyph.bitmap_top,
+              bitmap.buffer)
+    g.print()
+    g.even_h()
+    g.print()
+    print()
+    g.squash()
+    g.reduce_colors()
+    g.print()
+    print()
