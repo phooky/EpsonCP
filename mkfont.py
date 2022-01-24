61962395d413ddbbcbab95c6abc643aec3224587
phooky <phooky@gmail.com>
2022-01-24 14:38:29 -0500

    cleaning up mkfont and prepping for dynamic font loading

--- mkfont.py
+++ mkfont.py
@@ -6,17 +6,6 @@ import subprocess
 import struct
 from base64 import b64encode
 
-fn = 'hack'
-if len(sys.argv) > 1:
-    fn = sys.argv[1]
-
-lookup_result = subprocess.run("fc-match -f %{{file}} {}".format(fn).split(),
-                               capture_output=True,
-                               text=True)
-if lookup_result.returncode != 0:
-    print("Couldn't find font, exiting")
-    sys.exit(1)
-
 class Glyph:
     def __init__(self,w,h,left,top,bitmap):
         self.w = w
@@ -24,15 +13,16 @@ class Glyph:
         self.left = left
         self.top = top
         self.bitmap = bitmap
-        self.aspect = 1
 
     def even_h(self):
+        "Modify glyph to support an even number of rows."
         if (self.h%2) == 1:
             self.h = self.h + 1
             self.top = self.top + 1
             self.bitmap = ([0] * self.w) + self.bitmap
 
     def squash(self):
+        "Squash font on vertical axis by a factor of 2."
         self.h = int(self.h/2)
         self.top = int(self.top/2)
         bm = [0] * int(self.w * self.h)
@@ -41,32 +31,15 @@ class Glyph:
                 bm[x+y*self.w] = 0
                 bm[x+y*self.w] = int( (self.bitmap[x+(y*2*self.w)] +
                                        self.bitmap[x+((y*2+1)*self.w)])/2)
-        self.aspect = 2
         self.bitmap = bm
 
     def reduce_colors(self):
+        "Reduce bitmap to 2bpp."
         self.bitmap = [ min((x + 0x1f),0xff) >> 6 for x in self.bitmap ]
     
-    def print(self):
-        print("len {} x {} y {} left {} top {}".format(len(self.bitmap),self.w,self.h,
-                                                       self.left, self.top))
-        for y in range(self.h):
-            for x in range(self.w):
-                print("{:4}".format(self.bitmap[x + self.w*y]),end='')
-            if y == self.top - 1:
-                print(" -- baseline")
-            else:
-                print()
-
-    def pad_top(self,h):
-        if (h <= self.top):
-            return
-        delta = h - self.top
-        self.bitmap = ([0] * (self.w * delta)) + self.bitmap
-        self.h = self.h + delta
-        self.top = h
         
     def kitty(self):
+        "Write modified glyph to terminal via Kitty graphical codes."
         b = b'' #b'\00\00\00' * self.left
         for y in range(self.h):
             st = self.w * y
@@ -80,6 +53,7 @@ class Glyph:
         sys.stdout.flush()
 
     def pack_2bpp(self):
+        "Return bitmap data in 2bpp representation."
         bytew = int( (self.w+3)/4 )
         packed = bytearray()
         for y in range(self.h):
@@ -91,68 +65,6 @@ class Glyph:
                 row[byteidx] |= src << (pixidx*2)
             packed += row
         return packed
-        
-class EpsonFont:
-    def __init__(self, face, ptsize):
-        self.ascii_map = [None] * 128
-        self.face = face
-        self.face.set_char_size( ptsize * 64)
-    def add_glyph(self, char):
-        cs = char.encode('ascii')
-        if len(cs) > 1 or cs[0] > 127:
-            print("Character {} is out of ASCII range".format(char))
-            return
-        self.face.load_char(char)
-        bitmap = self.face.glyph.bitmap
-        g = Glyph(bitmap.width, bitmap.rows,
-                  self.face.glyph.bitmap_left,
-                  self.face.glyph.bitmap_top,
-                  bitmap.buffer.copy())
-        if char == ' ':
-            g.w = self.face.glyph.metrics.horiAdvance >> 6
-        g.even_h()
-        g.squash()
-        g.reduce_colors()
-        self.ascii_map[cs[0]] = g
-        #sys.stdout.write("Inserting {} ({}/{}) :".format(char,g.top,g.h))
-        #sys.stdout.flush()
-        #g.kitty()
-        #print()
-
-    def write_font(self):
-        min_ch = len(self.ascii_map)
-        max_ch = -1
-        ascender = 0
-        descender = 0
-        for (i,g) in enumerate(self.ascii_map):
-            if not g:
-                continue
-            min_ch,max_ch = min(min_ch,i),max(max_ch,i)
-            ascender = max(ascender,g.top)
-            descender = max(descender,g.h-g.top)
-        baseline = ascender
-        height = ascender+descender
-        if min_ch > max_ch:
-            print("Font is empty")
-            return None
-        #print("First {}, last {}, baseline {}, height {}".format(min_ch,max_ch,
-        #                                                         baseline, height))
-        data = bytearray([baseline,height,min_ch,max_ch])
-        data += bytes([0,0] * (1 + max_ch - min_ch))
-        for i in range(min_ch, max_ch+1):
-            lookup_off = 4 + (i - min_ch)*2
-            data[lookup_off:lookup_off+2] = struct.pack("<H",len(data))
-            g = self.ascii_map[i]
-            if g:
-                top = height - g.top
-                bot = g.h + top
-                pw = g.w
-                bw = int( (g.w+3)/4 )
-                data += bytearray([top,bot,pw,bw])
-                data += g.pack_2bpp()
-        return data
-
-            
 
 
 #| offset | name       | size | description |
@@ -170,62 +82,75 @@ class EpsonFont:
 #| + 2 | pw   | 1 | width in pixels of a line
 #| + 3 | bw   | 1 | width in bytes of a line
 #| + 4 | data | bw*(bot-top) | packed character data (4 pix / byte)
-        
-
-# test retrieving a glyph
-def getglyph(font,char):
-    code = char.encode('ascii')[0]
-    fc, lc = font[2],font[3]
-    if code < fc or code > lc:
-        print("char out of range: {}".format(char))
-        return
-    height = font[1]
-    lidx = 4 + ((code - fc)*2)
-    gidx = (font[lidx+1] << 8) + font[lidx]
-    width = font[gidx+2]
-    # kitty output
-    b = bytearray(b'\033_Gf=24,')
-    b += 's={},v={},a=T;'.format(width, height*2).encode('ascii')
-    top,bot = font[gidx], font[gidx+1]
-    pw,bw = font[gidx+2], font[gidx+3]
-    d = bytearray()
-    for y in range(height):
-        for _ in range(2):
-            if y < top or y >= bot:
-                d += b'\00\00\00' * width
-            else:
-                lineidx = gidx + 4 + (y-top)*bw
-                for x in range(pw):
-                    byoff = int(x/4)
-                    bioff = (x%4) * 2
-                    v = (font[lineidx + byoff] >> bioff) & 0x03
-                    d.append(v * 85)
-                    d.append(v * 85)
-                    d.append(v * 85)
-    b += b64encode(d)
-    b += b'\033'
-    b += b'\\'
-    sys.stdout.buffer.write(bytes(b))
-    sys.stdout.flush()
-
-
-import string
-font_path = lookup_result.stdout;
-face = freetype.Face(font_path)
-ef = EpsonFont(face,24)
-for c in string.ascii_lowercase + string.ascii_uppercase + " .'!":
-    ef.add_glyph(c)
-font = ef.write_font()
-
-fontstr = ''
-while font:
-    fontstr += '    '
-    line = font[:12]
-    font = font[12:]
-    ldat = ', '.join(map(lambda x:'0x{:02x}'.format(x), line))
-    fontstr += ldat + ',\n'
     
-print("""
+class EpsonFont:
+    def __init__(self, face, pxsize):
+        self.ascii_map = [None] * 128
+        self.face = face
+        self.face.set_char_size( pxsize * 64 ) # adjust to 16.6 fixed-point format
+        
+    def add_glyph(self, char):
+        cs = char.encode('ascii')
+        if len(cs) > 1 or cs[0] > 127:
+            print("Character {} is out of ASCII range".format(char))
+            return
+        self.face.load_char(char)
+        bitmap = self.face.glyph.bitmap
+        g = Glyph(bitmap.width, bitmap.rows,
+                  self.face.glyph.bitmap_left,
+                  self.face.glyph.bitmap_top,
+                  bitmap.buffer.copy())
+        if char == ' ': # spaces have empty bitmaps, just advance the cursor
+            g.w = self.face.glyph.metrics.horiAdvance >> 6 # it's in 16.6 fixed-point format
+        g.even_h()
+        g.squash()
+        g.reduce_colors()
+        self.ascii_map[cs[0]] = g
+
+    def write_font(self):
+        "Return total font as a bytearray"
+        min_ch = len(self.ascii_map)
+        max_ch = -1
+        ascender = 0
+        descender = 0
+        for (i,g) in enumerate(self.ascii_map):
+            if not g:
+                continue
+            min_ch,max_ch = min(min_ch,i),max(max_ch,i)
+            ascender = max(ascender,g.top)
+            descender = max(descender,g.h-g.top)
+        baseline = ascender
+        height = ascender+descender
+        if min_ch > max_ch:
+            print("Font is empty")
+            return None
+        data = bytearray([baseline,height,min_ch,max_ch])
+        data += bytes([0,0] * (1 + max_ch - min_ch))
+        for i in range(min_ch, max_ch+1):
+            lookup_off = 4 + (i - min_ch)*2
+            data[lookup_off:lookup_off+2] = struct.pack("<H",len(data))
+            g = self.ascii_map[i]
+            if g:
+                top = height - g.top
+                bot = g.h + top
+                pw = g.w
+                bw = int( (g.w+3)/4 )
+                data += bytearray([top,bot,pw,bw])
+                data += g.pack_2bpp()
+        return data
+
+import argparse
+import string
+
+def font_as_C_header(font):
+    fontstr = ''
+    while font:
+        fontstr += '    '
+        line = font[:12]
+        font = font[12:]
+        ldat = ', '.join(map(lambda x:'0x{:02x}'.format(x), line))
+        fontstr += ldat + ',\n'
+    return """
 #ifndef __FONT_{GUARD}_H__
 #define __FONT_{GUARD}_H__
 
@@ -236,4 +161,25 @@ uint8_t font_{FN}[] = {{
 }}
 
 #endif // __FONT_{GUARD}_H__
-""".format(GUARD=fn.upper(),FN=fn,DATA=fontstr))
+""".format(GUARD=args.font.upper(),FN=args.font,DATA=fontstr)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate EpsonCP fonts from freetype fonts.')
+    parser.add_argument('font', default='hack', help='Name of the font to process')
+    parser.add_argument('size', type=int, default=24, nargs='?', help='Size in pixels of font')
+    args = parser.parse_args()
+    lookup_result = subprocess.run(['fc-match', '-f', '%{file}', args.font ],
+                                   capture_output=True,
+                                   text=True)
+    if lookup_result.returncode != 0:
+        print("Couldn't find font, exiting")
+        sys.exit(1)
+
+    font_path = lookup_result.stdout;
+    face = freetype.Face(font_path)
+    ef = EpsonFont(face,args.size)
+    for c in string.ascii_lowercase + string.ascii_uppercase + string.digits + " .'!":
+        ef.add_glyph(c)
+    font = ef.write_font()
+    sys.stderr.write("Font {}, binary size {}\n".format(args.font,len(font)))
+    print(font_as_C_header(font))
